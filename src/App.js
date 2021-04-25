a9a690a06e3132a856adf1ddc7dbcb469ae2836b
junaama <0naama0@gmail.com>
2021-04-25 12:54:26 -0400

    update videos

--- src/App.js
+++ src/App.js
@@ -1,41 +1,44 @@
-import React, {Component} from 'react';
-import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
+import React, { Component } from "react";
+import SideNav, {
+  NavItem,
+  NavIcon,
+  NavText,
+} from "@trendmicro/react-sidenav";
 
 // Be sure to include styles at some point, probably during your bootstraping
-import '@trendmicro/react-sidenav/dist/react-sidenav.css';
-import Login from './components/login.js'
-import './App.css';
+import "@trendmicro/react-sidenav/dist/react-sidenav.css";
+import Login from "./components/login.js";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div>
-       <SideNav
-    onSelect={(selected) => {
-        // Add your code here
-    }}
->
-    <SideNav.Toggle />
-    <SideNav.Nav defaultSelected="home">
-        <NavItem eventKey="home">
-            <NavIcon>
-                <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
-            </NavIcon>
-            <NavText>
-                Home
-            </NavText>
-        </NavItem>
-        <NavItem eventKey="charts">
-            <NavIcon>
-                <i className="fa fa-fw " style={{ fontSize: '1.75em' }} />
-            </NavIcon>
-            <NavText>
-                Forum
-            </NavText>
-        </NavItem>
-    </SideNav.Nav>
-</SideNav>
-        <Login/>
+        <SideNav
+          onSelect={(selected) => {
+            // Add your code here
+          }}
+        >
+          <SideNav.Toggle />
+          <SideNav.Nav defaultSelected="home">
+            <NavItem eventKey="home">
+              <NavIcon>
+                <i
+                  className="fa fa-fw fa-home"
+                  style={{ fontSize: "1.75em" }}
+                />
+              </NavIcon>
+              <NavText>Home</NavText>
+            </NavItem>
+            <NavItem eventKey="charts">
+              <NavIcon>
+                <i className="fa fa-fw " style={{ fontSize: "1.75em" }} />
+              </NavIcon>
+              <NavText>Forum</NavText>
+            </NavItem>
+          </SideNav.Nav>
+        </SideNav>
+        <Login />
       </div>
     );
   }
