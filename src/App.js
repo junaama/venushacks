b1142d01cad5fe736f8f1d613413c7e05b814067
rubleen1903 <rubleen16@gmail.com>
2021-04-25 18:09:39 +0530

    Updated LoginPage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
+import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
 
+// Be sure to include styles at some point, probably during your bootstraping
+import '@trendmicro/react-sidenav/dist/react-sidenav.css';
 import Login from './components/login.js'
 import './App.css';
 
@@ -7,7 +10,31 @@ class App extends Component {
   render() {
     return (
       <div>
-       
+       <SideNav
+    onSelect={(selected) => {
+        // Add your code here
+    }}
+>
+    <SideNav.Toggle />
+    <SideNav.Nav defaultSelected="home">
+        <NavItem eventKey="home">
+            <NavIcon>
+                <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
+            </NavIcon>
+            <NavText>
+                Home
+            </NavText>
+        </NavItem>
+        <NavItem eventKey="charts">
+            <NavIcon>
+                <i className="fa fa-fw " style={{ fontSize: '1.75em' }} />
+            </NavIcon>
+            <NavText>
+                Forum
+            </NavText>
+        </NavItem>
+    </SideNav.Nav>
+</SideNav>
         <Login/>
       </div>
     );
