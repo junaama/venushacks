13866bc18137ebb14f33fe5b2f8fdbbe80900a81
junaama <0naama0@gmail.com>
2021-04-24 22:58:37 -0400

    finish forum/video api

--- server/routes/forum.js
+++ server/routes/forum.js
@@ -7,8 +7,8 @@ const awaitHandlerFactory = require('../middleware/handler');
 router.get('/', awaitHandlerFactory(forumController.getAllForums)); // localhost:3000/api/v1/forums
 router.get('/id/:id', awaitHandlerFactory(forumController.getForumById)); // localhost:3000/api/v1/forums/id/vid
 router.get('/forums/:title', awaitHandlerFactory(forumController.getForumByTitle)); // localhost:3000/api/v1/forums/forumtitle
-router.post('/', createForumSchema, awaitHandlerFactory(forumController.createForum)); // localhost:3000/api/v1/forums
-router.patch('/id/:id',updateForumSchema, awaitHandlerFactory(forumController.updateForum)); // localhost:3000/api/v1/forums/id/vid , using patch for partial update
+router.post('/', awaitHandlerFactory(forumController.createForum)); // localhost:3000/api/v1/forums
+router.patch('/id/:id', awaitHandlerFactory(forumController.updateForum)); // localhost:3000/api/v1/forums/id/vid , using patch for partial update
 router.delete('/id/:id', awaitHandlerFactory(forumController.deleteForum)); // localhost:3000/api/v1/forums/id/vid
 
 module.exports = router;
--- server/routes/video.js
+++ server/routes/video.js
@@ -7,8 +7,7 @@ const awaitHandlerFactory = require('../middleware/handler');
 router.get('/', awaitHandlerFactory(videoController.getAllVideos)); // localhost:3000/api/v1/videos
 router.get('/id/:id', awaitHandlerFactory(videoController.getVideoById)); // localhost:3000/api/v1/videos/id/vid
 router.get('/videos/:title', awaitHandlerFactory(videoController.getVideoByTitle)); // localhost:3000/api/v1/videos/videotitle
-router.post('/', createVideoSchema, awaitHandlerFactory(videoController.createVideo)); // localhost:3000/api/v1/videos
-router.patch('/id/:id',updateVideoSchema, awaitHandlerFactory(videoController.updateVideo)); // localhost:3000/api/v1/videos/id/vid , using patch for partial update
+router.post('/', awaitHandlerFactory(videoController.createVideo)); // localhost:3000/api/v1/videos
+router.patch('/id/:id', awaitHandlerFactory(videoController.updateVideo)); // localhost:3000/api/v1/videos/id/vid , using patch for partial update
 router.delete('/id/:id', awaitHandlerFactory(videoController.deleteVideo)); // localhost:3000/api/v1/videos/id/vid
-
 module.exports = router;
